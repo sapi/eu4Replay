a0baaffe0600d05523a18b569c00f2f3a1bdf042
sapi <sean.purdon@gmail.com>
2014-07-08 21:59:42 -0700

    Addded further tests covering object parsing

--- tests/parsers/saves.py
+++ tests/parsers/saves.py
@@ -1,4 +1,4 @@
-from io import IOBase
+from datetime import datetime
 from StringIO import StringIO
 import unittest
 
@@ -120,7 +120,14 @@ class ParseObjectTests(unittest.TestCase):
             self.assertIsInstance(result, list)
             self.assertListEqual(result, expected)
 
+    def checkIsInvalid(self, s):
+        stream = StringIO(s)
+        result = parse_object(stream)
+
+        self.assertIsNone(result)
+
     ## Valid objects: tests should pass
+    # Simple dicts
     def testSingleKeyValuePairIsValid(self):
         s = '''
                 key=value
@@ -143,6 +150,21 @@ class ParseObjectTests(unittest.TestCase):
 
         self.checkIsValid(s, expected)
 
+    def testTwoKeyValuePairsWithBlankLineAreValid(self):
+        s = '''
+                key=value
+                
+                key2=value2
+            '''
+
+        expected = {
+                'key': 'value',
+                'key2': 'value2',
+                }
+
+        self.checkIsValid(s, expected)
+
+    # Simple arrays
     def testSingleElementArrayIsValid(self):
         s = '''
                 item
@@ -187,3 +209,273 @@ class ParseObjectTests(unittest.TestCase):
                 ]
 
         self.checkIsValid(s, expected)
+
+    # Stupid formats
+    def testDictWithSpacesAroundEqualsIsValid(self):
+        s = '''
+                key = value
+            '''
+        expected = {
+                'key': 'value',
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithSpacesAfterValueBeforeNewlineIsValid(self):
+        s = '''
+                key=value     
+                key2=value2
+            '''
+        expected = {
+                'key': 'value',
+                'key2': 'value2',
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithIntValueIsValid(self):
+        s = '''
+                key=0
+            '''
+        expected = {
+                'key': 0,
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithFloatValueIsValid(self):
+        s = '''
+                key=0.1
+            '''
+        expected = {
+                'key': 0.1,
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithStringValueIsValid(self):
+        s = '''
+                key="value"
+            '''
+        expected = {
+                'key': 'value',
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithDateValueIsValid(self):
+        s = '''
+                key="1444.1.1"
+            '''
+        expected = {
+                'key': datetime(1444, 1, 1),
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithBooleanTrueIsValid(self):
+        s = '''
+                key=yes
+            '''
+        expected = {
+                'key': True,
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictWithBooleanFalseIsValid(self):
+        s = '''
+                key=no
+            '''
+        expected = {
+                'key': False,
+                }
+
+        self.checkIsValid(s, expected)
+
+    # Nested objects
+    def testDictInsideDictIsValid(self):
+        s = '''
+                key={
+                    key=value
+                }
+            '''
+        expected = {
+                'key': {
+                    'key': 'value',
+                    },
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictInsideDictInsideDictIsValid(self):
+        s = '''
+                key={
+                    key={
+                        key=value
+                    }
+                }
+            '''
+        expected = {
+                'key': {
+                    'key': {
+                        'key': 'value',
+                        },
+                    },
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testArrayInsideDictIsValid(self):
+        s = '''
+                key={ one two three four }
+            '''
+        expected = {
+                'key': ['one', 'two', 'three', 'four']
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictInsideDictWithBraceOnNewLineIsValid(self):
+        s = '''
+                key=
+                {
+                    key=value
+                }
+            '''
+        expected = {
+                'key': {
+                    'key': 'value',
+                    },
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictInsideDictWithSpacesBeforeBraceIsValid(self):
+        s = '''
+                key=      {
+                    key=value
+                }
+            '''
+        expected = {
+                'key': {
+                    'key': 'value',
+                    },
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testDictInsideDictWithSpacesAfterBraceIsValid(self):
+        s = '''
+                key={     
+                    key=value
+                }
+            '''
+        expected = {
+                'key': {
+                    'key': 'value',
+                    },
+                }
+
+        self.checkIsValid(s, expected)
+
+    # Repeated keys (oh, god, why is this possible Paradox?!)
+    def testRepeatedKeyValuePairsMergeAndAreValid(self):
+        s = '''
+                key=one
+                key=two
+            '''
+        expected = {
+                'key': ['one', 'two'],
+            }
+
+        self.checkIsValid(s, expected)
+
+    def testRepeatedKeysWithDictValuesMergeAndAreValid(self):
+        s = '''
+                key={
+                    one=1
+                }
+                key={
+                    two=2
+                }
+            '''
+        expected = {
+                'key': {
+                    'one': 1,
+                    'two': 2,
+                    }
+                }
+
+        self.checkIsValid(s, expected)
+
+    # Invalid child objects
+    def testObjectWithInvalidChildObjectIsValid(self):
+        s = '''
+                key={
+                    key=
+                }
+            '''
+        expected = {
+                'key': None,
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testObjectWithEmptyChildObjectIsValid(self):
+        s = '''
+                key = { }
+            '''
+        expected = {
+                'key': None,
+                }
+
+        self.checkIsValid(s, expected)
+
+    def testObjectWithExtraneousChildObjectIsValid(self):
+        # this comes up quite often, but god knows why...
+        s = '''
+                key=value
+                {
+                }
+                key2 = {
+                    key=value
+                }
+                {
+                }
+            '''
+        expected = {
+                'key': 'value',
+                'key2': {
+                    'key': 'value',
+                    }
+                }
+
+        self.checkIsValid(s, expected)
+
+    ## Invalid objects: tests should fail
+    def testEmptyObjectIsInvalid(self):
+        s = ''
+
+        self.checkIsInvalid(s)
+
+    def testObjectWithKeyButNoValueIsInvalid(self):
+        s = 'key='
+
+        self.checkIsInvalid(s)
+
+    def testObjectWithKeyValuePairThenHangingStringIsInvalid(self):
+        s = '''
+                key=value
+                key2
+            '''
+
+        self.checkIsInvalid(s)
+
+    def testObjectWithKeyValuePairThenKeyButNoValueIsInvalid(self):
+        s = '''
+                key=value
+                key2=
+            '''
+
+        self.checkIsInvalid(s)
